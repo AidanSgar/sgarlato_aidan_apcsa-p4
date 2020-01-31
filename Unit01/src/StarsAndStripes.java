4d4a29946979011ffe89e88228049939cb22e477
AidanSgar <aidanrsgarlato@gmail.com>
2020-01-31 14:51:57 -0800

    Unit 01: Methods and Object

---
+++ Unit01/src/StarsAndStripes.java
@@ -0,0 +1,50 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+
+   public StarsAndStripes()
+   {
+      System.out.println("StarsAndStripes");
+      System.out.println("");
+      System.out.println("");
+           
+   }
+
+   public void Stars()
+   {
+	   System.out.println("********************");
+   }
+
+   public void Dashes()
+   {
+	   System.out.println("--------------------");
+   }
+
+   public void printTwoBlankLines()
+   {
+	   System.out.println("");
+	   System.out.println("");
+   }
+   
+   public void printASmallBox()
+   {	
+	   System.out.println("[]");
+   }
+ 
+   public void printABigBox()
+   { 	
+	   System.out.println(" ______ ");
+	   System.out.println("|      |");
+	   System.out.println("|      |");
+	   System.out.println("|______|");
+   }   
+}
---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,28 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {     
+     StarsAndStripes sas = new StarsAndStripes();
+     
+     
+     sas.Dashes();
+     sas.Stars();
+     sas.Dashes();
+     sas.Stars();
+     sas.Dashes();
+     sas.Stars();
+     sas.Dashes();
+     sas.printTwoBlankLines();
+     sas.printABigBox();
+   }
+}
