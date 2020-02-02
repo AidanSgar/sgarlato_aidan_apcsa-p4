c96aa0d1afedd57e8cc676d0dc0f8529c4962bcc
AidanSgar <aidanrsgarlato@gmail.com>
2020-02-02 14:06:58 -0800

    Unit 02

---
+++ Unit02/src/Cube.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name -
+//Date -
+
+
+public class Cube
+{
+	public static int area( int s )
+	{
+		return 6*(s*s);
+				
+	}
+}
---
+++ Unit02/src/TrapRunner.java
@@ -0,0 +1,31 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class TrapRunner
+{
+	public static void main( String[] args )
+	{
+		System.out.println( Trap.area( 3.0, 3.0, 3.0) );
+		System.out.println( Trap.area( 5.0, 6.0, 7.0) );
+		System.out.println( Trap.area( 7.0, 10.0, 6.0) );
+		System.out.println( Trap.area( 13.0, 9.0, 3.0) );
+		System.out.println( Trap.area( 6.0, 11.0, 4.0) );
+		System.out.println( Trap.area( 11.0, 8.0, 5.0) );
+	}
+}
+
+
+/*
+EXPECTED OUTPUT
+
+9.0
+38.5
+51.0
+33.0
+34.0
+47.5
+
+*/
